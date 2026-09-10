291071ed3319306a3adcc72da79b664a8e5fab93
Bartjan Henkemans <bartjan@henkemans.be>
2026-09-10 11:40:12 +0200

    feat: languages for EAPC

--- docker/judgehost/chroot-and-tar.sh
+++ docker/judgehost/chroot-and-tar.sh
@@ -2,8 +2,25 @@
 
 set -euo pipefail
 
-# Usage: https://github.com/DOMjudge/domjudge/blob/main/misc-tools/dj_make_chroot.in#L58-L87
-/opt/domjudge/judgehost/bin/dj_make_chroot
+RELEASE=resolute #Contains the more up to date releases
+CHROOTDIR=/chroot/domjudge
+ln -sf gutsy "/usr/share/debootstrap/scripts/$RELEASE"
+sed -i 's/default-jdk-headless default-jre-headless/openjdk-21-jdk-headless openjdk-21-jre-headless/g' \
+	/opt/domjudge/judgehost/bin/dj_make_chroot
+
+/opt/domjudge/judgehost/bin/dj_make_chroot -D Ubuntu -R "$RELEASE"
+
+KOTLIN_VERSION=2.4.20
+KOTLIN_SHA256=59e9ca74c7904ef2c122b12114937673ccce68de820a663f0ed66ccf8799e0b7
+
+echo "[..] Installing Kotlin $KOTLIN_VERSION into the chroot"
+wget -q "https://github.com/JetBrains/kotlin/releases/download/v$KOTLIN_VERSION/kotlin-compiler-$KOTLIN_VERSION.zip" \
+	-O /kotlin-compiler.zip
+echo "$KOTLIN_SHA256  /kotlin-compiler.zip" | sha256sum -c -
+unzip -qq -d "$CHROOTDIR/usr/local/lib/" /kotlin-compiler.zip
+chroot "$CHROOTDIR" ln -s /usr/local/lib/kotlinc/bin/kotlinc /usr/local/bin/kotlinc
+rm /kotlin-compiler.zip
+
 
 cd /
 echo "[..] Compressing chroot"
